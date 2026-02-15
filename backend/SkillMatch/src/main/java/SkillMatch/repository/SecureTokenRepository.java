2dd24b9fc29937ccfddbb0a87d5ba0f41cbc7a58
Mamadou8Bah <mbah18791@gmail.com>
2026-02-15 13:29:05 +0000

    Fix missing imports in SecureTokenRepository and DefaultEmailService

--- backend/SkillMatch/src/main/java/SkillMatch/repository/SecureTokenRepository.java
+++ backend/SkillMatch/src/main/java/SkillMatch/repository/SecureTokenRepository.java
@@ -1,5 +1,6 @@
 package SkillMatch.repository;
 
+import SkillMatch.model.SecureToken;
 import SkillMatch.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
